2693b2176fefbbe7599963ccea4baa01806ae004
farman17 <ahmadfarman1701@gmail.com>
2022-08-06 19:49:55 +0700

    update dashboard-server

--- GRAFANA/dashboard-server-monitoring.sh
+++ GRAFANA/dashboard-server-monitoring.sh
@@ -68,11 +68,14 @@ echo
 clear
 echo "Setting target untuk node yang ingin di monitoring di prometheus:"
 echo
+echo -n "masukkan nama job untuk prometehus: ";
+read job;
+echo
 echo -n "masukkan IP Node yang ingin di pantau: ";
 read ipnode;
 echo
 sed -i '$ a\ ' /etc/prometheus/prometheus.yml
-sed -i '$ a\  - job_name: node_exporter_server_monitoring' /etc/prometheus/prometheus.yml
+sed -i '$ a\  - job_name: $job' /etc/prometheus/prometheus.yml
 sed -i '$ a\    static_configs:' /etc/prometheus/prometheus.yml 
 sed -i '$ a\      - targets: ['$ipnode:9100']' /etc/prometheus/prometheus.yml
 echo
