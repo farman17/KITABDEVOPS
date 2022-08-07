a51f69f6f3dc29581a7a142671529e14e0de3456
farman17 <ahmadfarman1701@gmail.com>
2022-08-07 10:43:25 +0700

    adding http-monitoring

---
+++ GRAFANA/http-monitoring.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+clear
+
+echo "Add Blackbox exporter (WORKER ONLY)"
+sudo useradd -M -r -s /bin/false blackbox
+curl -s https://api.github.com/repos/prometheus/blackbox_exporter/releases/latest | grep browser_download_url | grep linux-amd64 | cut -d '"' -f 4 | wget -qi -
+
+tar xvzf blackbox_exporter-*.linux-amd64.tar.gz
+cd blackbox_exporter-*.linux-amd64
+sudo cp blackbox_exporter /usr/local/bin
+blackbox_exporter --version
+sudo chown blackbox:blackbox /usr/local/bin/blackbox_exporter
+cd ~/blackbox_exporter-*.linux-amd64
+sudo mkdir -p /etc/blackbox
+sudo cp blackbox.yml /etc/blackbox
+sudo chown -R blackbox:blackbox /etc/blackbox/*
+
