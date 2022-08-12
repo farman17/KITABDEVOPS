2e0984f96a063084cbedcd995b25db9a8bd60a82
farman17 <69154746+farman17@users.noreply.github.com>
2022-08-12 13:28:12 +0700

    Create k3s.sh

---
+++ KUBERNETES/k3s.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#SUMBER : https://itnext.io/setup-your-own-kubernetes-cluster-with-k3s-b527bf48e36a
+#https://gladi.id/k3s-vs-k8s-apa-perbadinganya/
+curl -sfL https://get.k3s.io | sh -
+#Get the node token which is needed in the next step:
+#cat /var/lib/rancher/k3s/server/node-token
+#On each machine that is going to be a worker node (JOINT WORKER) :
+#curl -sfL https://get.k3s.io | K3S_URL=https://kubernetes01.domain.de:6443 K3S_TOKEN=<the-token-from-the-step-before> sh -
+clear
+sudo k3s kubectl get node
+
+echo "SELESAI........"
