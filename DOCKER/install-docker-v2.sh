8b9383d6d27954804538d7f7bc4349924fcde336
farman17 <69154746+farman17@users.noreply.github.com>
2022-08-12 11:24:00 +0700

    Update install-docker-v2.sh

--- DOCKER/install-docker-v2.sh
+++ DOCKER/install-docker-v2.sh
@@ -1,4 +1,6 @@
-
+#!/bin/bash
+clear
+echo
 sudo apt update
 sudo apt install apt-transport-https ca-certificates curl software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
