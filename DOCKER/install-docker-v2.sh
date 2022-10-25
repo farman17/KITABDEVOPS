0ce6551d88a44537589fb26c122014283f6be891
farman17 <69154746+farman17@users.noreply.github.com>
2022-10-25 16:50:58 +0700

    Update install-docker-v2.sh

--- DOCKER/install-docker-v2.sh
+++ DOCKER/install-docker-v2.sh
@@ -2,15 +2,10 @@
 clear
 echo
 sudo apt update
+clear
 sudo apt install apt-transport-https ca-certificates curl software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
-
-sudo apt update
 apt-cache policy docker-ce
-
 sudo apt install docker-ce
-sudo usermod -aG docker ${USER}
-su - ${USER}
-id -nG
-sudo usermod -aG docker username
+sudo systemctl status docker
