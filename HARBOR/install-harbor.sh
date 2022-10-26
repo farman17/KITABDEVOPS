0bbaf9966951c369ebee76edff54b62ccfd9bc8b
farman17 <69154746+farman17@users.noreply.github.com>
2022-10-26 10:42:48 +0700

    Update install-harbor.sh

--- HARBOR/install-harbor.sh
+++ HARBOR/install-harbor.sh
@@ -10,3 +10,8 @@ wget https://github.com/goharbor/harbor/releases/download/v2.4.1/harbor-offline-
 tar -xvzf harbor-offline-installer-v2.4.1.tgz
 cd harbor
 cp harbor.yml.tmpl harbor.yml
+
+
+
+#source references :
+#https://www.cyberithub.com/how-to-install-harbor-on-ubuntu-20-04-lts-step-by-step/
