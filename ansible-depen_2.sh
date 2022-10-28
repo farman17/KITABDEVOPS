b111077fece421ac56ff94d90f961c9b25b485f3
farman17 <69154746+farman17@users.noreply.github.com>
2022-10-28 17:10:50 +0700

    Update and rename ansible-depen.sh to ansible-depen_2.sh

---
+++ ansible-depen_2.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+clear
+apt-get update
+clear
+apt-get install python3 ansible
+clear
+
+sudo ssh-keygen -t rsa
+cd /root/.ssh
+sudo chmod 400 id_rsa
+
+echo -n "masukkan domain dest-host: ";
+read host;
+echo -n "masukkan username dest-host: ";
+read user1;
+echo
+echo -n "masukkan domain source-host: ";
+read source;
+echo -n "masukkan username source-host: ";
+read user2;
+clear
+tee /etc/ansible/hosts<<EOF
+ [$host]
+ 192.168.33.33     ansible_user=$user1  ansible_ssh_private_key=/root/.ssh/id_rsa
+
+ [$source]
+ 192.168.33.9     ansible_user=$user2  ansible_ssh_private_key=/root/.ssh/id_rsa 
+EOF
+echo
+echo
+ssh-copy-id $user1@192.168.33.33
+echo
+echo
+ansible $host -m ping
+echo
+echo "DONE......."
