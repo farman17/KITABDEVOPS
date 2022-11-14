32ec9c76157c57bfce24873d5c76370d7cfd7c39
farman17 <69154746+farman17@users.noreply.github.com>
2022-11-14 15:14:32 +0700

    Update install-rancher.sh

--- RANCHER/install-rancher.sh
+++ RANCHER/install-rancher.sh
@@ -4,7 +4,7 @@ echo
 echo
 echo "**Install RANCHER Versi 2.4.18 di cloud**"
 echo
-docker run -d --name=rancher-server --restart=unless-stopped -p 80:80 -p 443:443 --privileged rancher/rancher:v2.4.18
+docker run -d --name=rancher-server --restart=unless-stopped -p 3001:80 -p 443:443 --privileged rancher/rancher:v2.4.18
 echo
 clear
 echo "Cek Installasi apakah image rancher sudah ter-pull dan running di docker :"
