0fa17e311b4f57b5bab7da9508cfbd4c771e06ab
farman17 <69154746+farman17@users.noreply.github.com>
2024-02-15 16:03:22 +0700

    Update worker-first.sh

--- ANSIBLE/worker-first.sh
+++ ANSIBLE/worker-first.sh
@@ -15,13 +15,9 @@ usermod -aG sudo $username
 
 runuser -l  $username  -c  'sudo ssh-keygen -t rsa'
 runuser -l  $username  -c  'sudo chmod 400 /root/.ssh/id_rsa'
-#sudo ssh-keygen -t rsa
-#cd /root/.ssh
-#sudo chmod 400 id_rsa
 
-echo -n "masukkan domain dest-host: ";
-read host1;
-echo -n "masukkan IP dest-host: ";
-read ip1;
-echo -n "masukkan username dest-host: ";
-read user1;
+sudo ssh-keygen -t rsa
+cd /root/.ssh
+sudo chmod 400 id_rsa
+
+
