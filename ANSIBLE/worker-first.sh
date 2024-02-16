9e20f863ab60e564a03f64b32a536febf43ce370
farman17 <69154746+farman17@users.noreply.github.com>
2024-02-16 10:55:44 +0700

    Update worker-first.sh

--- ANSIBLE/worker-first.sh
+++ ANSIBLE/worker-first.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 clear
 apt-get update
+echo "install open ssh untuk remote"
 apt install openssh-server
 #sudo apt install -y software-properties-common
 #sudo add-apt-repository --yes --update ppa:ansible/ansible
@@ -19,4 +20,5 @@ runuser -l  $username  -c  'sudo chmod 400 /root/.ssh/id_rsa'
 sudo ssh-keygen -t rsa
 cd /root/.ssh
 sudo chmod 400 id_rsa
+echo "%sudo  ALL=(ALL:ALL) NOPASSWD: ALL" >> /etc/sudoers
 echo "DONE"
